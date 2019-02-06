e1b265872ed9501c54cbe2695f4d91b14ebccf3c
Jaguso <jagutzo@gmail.com>
2019-02-06 02:49:49 -0600

    configured routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CreateAuthorsComponent } from './components/create-authors/create-authors.component';
 import { ListAuthorsComponent } from './components/list-authors/list-authors.component';
+
+const routes: Routes = [
+  { path: '', component: CreateAuthorsComponent },
+  { path: 'authorslist', component: ListAuthorsComponent}
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +21,8 @@ import { ListAuthorsComponent } from './components/list-authors/list-authors.com
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
